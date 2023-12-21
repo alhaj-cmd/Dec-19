bc9050a3dea9d515c0c57375dd7c3dadd668dfd4
alhaj sarkar <you@domain.com>
2023-12-21 22:44:44 +0600

    or symble setting

--- problemIf.js
+++ problemIf.js
@@ -1,10 +1,10 @@
 const naMe = "Rohim";
-const yoY = 10;
+const yoY = 101;
  
-if (naMe == 'Rohim' || yoY == 10 ){
+if (naMe == 'Rohims' || yoY == 10 ){
 console.log('Rohim is a good boy');}
 
-else if(naMe == 'tareks' && yoY == 10){
+else if(naMe == 'tareks' || yoY == 101){
 console.log('tarek is not good boy')}
 else {
     console.log('shofi is not a good chele')}
