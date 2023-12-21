90b58022f55ba817e1e4f79963f9c643a812e4ef
alhaj sarkar <you@domain.com>
2023-12-21 22:39:17 +0600

    problem solve

--- problemIf.js
+++ problemIf.js
@@ -1,9 +1,10 @@
-const naMe = "tarek";
+const naMe = "Rohim";
+const yoY = 10;
  
-if (naMe == 'Rohim' ){
+if (naMe == 'Rohim' && yoY == 10 ){
 console.log('Rohim is a good boy');}
 
-else if(naMe == 'tarek'){
+else if(naMe == 'tareks' && yoY == 10){
 console.log('tarek is not good boy')}
 else {
     console.log('shofi is not a good chele')}
