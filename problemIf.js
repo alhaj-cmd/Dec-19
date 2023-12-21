351fe070d71646ce67dcab67af24d71cd4513738
alhaj sarkar <you@domain.com>
2023-12-21 22:32:53 +0600

    problem two

---
+++ problemIf.js
@@ -0,0 +1,9 @@
+const naMe = "Rohim";
+ 
+if (naMe == 'Rohim' ){
+console.log('Rohim is a good boy');}
+
+else if(naMe == 'tarek'){
+console.log('tarek is not good boy')}
+else {
+    console.log('shofi is not a good chele')}
