0c49af9e28803a96b8dba40ea4201aa4de77dc64
alhaj sarkar <you@domain.com>
2023-12-23 00:23:21 +0600

    nested if else problem solving

--- problem3.js
+++ problem3.js
@@ -1,8 +1,14 @@
-const num = 30;
+const num = 300;
 
 
 if(num == 30){
     if (num == 'string'){
         console.log('num 30 is stirng')
     }
+    else{
+        console.log('num is not string')
+    }
+}
+else{
+    console.log('num is 30');
 }
